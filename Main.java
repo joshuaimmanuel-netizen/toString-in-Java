84de32ecee0f7a4b8484a000b1783519b6a79b48
joshuaimmanuel-netizen <106328263+joshuaimmanuel-netizen@users.noreply.github.com>
2022-07-31 22:54:24 +0530

    Add files via upload
    .

---
+++ Main.java
@@ -0,0 +1,9 @@
+//           String buffer
+
+
+class Main{
+    public static void main(String[] args){
+        StringBuffer a= new StringBuffer("joshua");
+        System.out.println(a.toString());
+    }
+}
